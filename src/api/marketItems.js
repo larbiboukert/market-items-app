d34d3172d41e33b6b52bd6a97795fd8d66abe7ce
Larbi <larbibk27@github.com>
2021-03-07 13:53:03 +0100

    clean to deploy

--- src/api/marketItems.js
+++ src/api/marketItems.js
@@ -2,5 +2,6 @@ import axios from "axios";
 
 export const getMarketItems = (setMarketItems) =>
   axios
-    .get("http://localhost:3000/fake-api/marketItems.json")
+    // .get("http://localhost:3000/fake-api/marketItems.json")
+    .get("https://market-items-app.netlify.app/fake-api/marketItems.json")
     .then((res) => setMarketItems(res.data));
